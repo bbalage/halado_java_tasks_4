b630c7c273cba7670dc9c0fe777059c8c372bfa6
Bolyki Balázs <bbalage0@gmail.com>
2021-05-21 14:29:38 +0200

    Lombok used to shorten code.

--- src/main/java/hu/meiit/haladojava/Product.java
+++ src/main/java/hu/meiit/haladojava/Product.java
@@ -1,34 +1,18 @@
 package hu.meiit.haladojava;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
 public class Product {
-    private String name;
-    private int price;
-    private String type; // értéke lehet "csoki" vagy "banan"
+    @Getter(AccessLevel.PROTECTED) @Setter(AccessLevel.PROTECTED) private String name;
+    @Getter @Setter private int price;
+    @Getter @Setter @ToString.Exclude private String type; // értéke lehet "csoki" vagy "banan"
     public Product(String name, int price) {
         super();
         this.name = name;
-        this.price = price;
-    }
-    public String getType() {
-        return type;
-    }
-    public void setType(String type) {
-        this.type = type;
-    }
-    @Override
-    public String toString() {
-        return "Product [name=" + name + ", price=" + price + "]";
-    }
-    protected String getName() {
-        return name;
-    }
-    protected void setName(String name) {
-        this.name = name;
-    }
-    public int getPrice() {
-        return price;
-    }
-    public void setPrice(int price) {
         this.price = price;
     }
     public void increasePrice(int percentage) {
